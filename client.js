2b8e0020677ac8535021dfaad2b6168ab06ac610
zakiguedi <zaki.guedi@gmail.com>
2023-02-15 22:21:00 -0500

    snake initials

--- client.js
+++ client.js
@@ -1,16 +1,18 @@
 const net = require("net");
-const {IP, port} = require('./constants');
+const readline = require("readline");
 const connect = function () {
   const conn = net.createConnection({
-    host:  "165.227.47.243",
-    port:  50541
+    host: "165.227.47.243",
+    port: 50541
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+   conn.setEncoding("utf8");
+  
   conn.on("connect", () => {
- console.log("connection successful");
-});
+    console.log("connection Successful");
+    conn.write("Name: MLY");
+  });
   return conn;
 };
 
