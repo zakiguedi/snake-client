398381f931ed0ed25908c8070e3501b44b418c66
zakiguedi <zaki.guedi@gmail.com>
2023-02-18 18:45:20 -0500

    sending messages

--- input.js
+++ input.js
@@ -1,17 +1,41 @@
 let connection;
- 
-const setupInput = function () {
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
+  stdin.setEncoding('utf8');
   stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
   return stdin;
 };
-
-const handleUserInput = function(key) {
+const handleUserInput = (key) => {
+  const stdout = process.stdout;
   if (key === '\u0003') {
+    stdout.write("Exited snek game. Bye bye.\n");
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+
+  if (key === "e") {
+    connection.write("Say: hi!");
+  }
+  if (key === "q") {
+    connection.write("Say: bye!");
   }
 };
 
-module.exports = { setupInput }; 
+module.exports = { setupInput };
