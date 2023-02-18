fe24cc203f9090f4ce47327c6dfed9e619b1c60a
zakiguedi <zaki.guedi@gmail.com>
2023-02-18 17:53:12 -0500

    input module

---
+++ input.js
@@ -0,0 +1,17 @@
+let connection;
+ 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput }; 
